4fb87811e3536fa2820654e4651c789940c6145b
afthamafer <37636774+afthamafer@users.noreply.github.com>
2021-01-20 15:24:40 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyCm2t_mADfjxepVmGVkEA6Rav01MheObtM",
-    authDomain: "companion-4fe15.firebaseapp.com",
-    databaseURL: "https://companion-4fe15-default-rtdb.europe-west1.firebasedatabase.app",
+    apiKey: "AIzaSyCm2t_mADfjxepVmGVkEA6Rav01MTEST",
+    authDomain: "TEST-4fe15.firebaseapp.com",
+    databaseURL: "https://companTEST-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "companion-4fe15",
     storageBucket: "companion-4fe15.appspot.com",
-    messagingSenderId: "878260412599",
-    appId: "1:878260412599:web:543613a3908a48d34aed32",
+    messagingSenderId: "878260TEST599",
+    appId: "1:878260412599:web:543613a3908a48dTEST32",
     measurementId: "G-TWNPJC35DN"
   };
   // Initialize Firebase
